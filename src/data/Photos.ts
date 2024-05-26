2599b1f2e2b897e7508590d6ce927a798b4a55ae
TecoAdamo <adamomateus478@gmail.com>
2024-05-26 20:36:59 -0300

    feat: img for cards

---
+++ src/data/Photos.ts
@@ -0,0 +1,8 @@
+const PHOTOS = {
+  image1: require("../assets/blazer.png"),
+  image2: require("../assets/headset.jpg"),
+  image3: require("../assets/macbook.jpg"),
+  image4: require("../assets/relógio.jpg"),
+};
+
+export default PHOTOS;
---
+++ src/data/index.ts
@@ -0,0 +1,8 @@
+import PHOTO1 from "../assets/blazer.png";
+
+export const products = [
+  { id: "1", image: PHOTO1 },
+  // { id: "2", image: PHOTOS.image2 },
+  // { id: "3", image: PHOTOS.image3 },
+  // Adicione mais produtos conforme necessário
+];
