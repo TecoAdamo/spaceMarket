d08d64658c9244f9daac064d9feb66dda12e575a
TecoAdamo <adamomateus478@gmail.com>
2024-05-26 20:39:21 -0300

    feat: products of section

---
+++ src/components/Groups.tsx
@@ -0,0 +1,36 @@
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  Text,
+  StyleSheet,
+} from "react-native";
+
+type Props = TouchableOpacityProps & {
+  name: string;
+  isActive: boolean;
+};
+
+export function Group({ name, isActive, ...rest }: Props) {
+  return (
+    <TouchableOpacity style={styles.btn}>
+      <Text
+        style={{ color: isActive ? "#0D6EFD" : "gray", padding: 4 }}
+        {...rest}
+      >
+        {name}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  btn: {
+    marginRight: 8,
+    marginTop: 18,
+    padding: 6,
+    marginLeft: 8,
+    height: 38,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 10,
+  },
+});
