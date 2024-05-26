110fdc29464332e21fe93b24a0eddbb3bd80449e
TecoAdamo <adamomateus478@gmail.com>
2024-05-26 20:43:21 -0300

    refatcor: added flatList with section

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,20 +1,59 @@
+import { useState } from "react";
+
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, View, Image, FlatList, Text } from "react-native";
 import SearchProducts from "../components/SearchProducts";
 
+import BANNER from "../assets/banner.png";
+import { Group } from "../components/Groups";
+import DealsOffers from "../components/DealsOffers";
+
+import { products } from "../data";
+
 export default function Home() {
+  const [groups, setGroups] = useState([
+    "Todas categorias",
+    "Celulares",
+    "Roupas",
+    "Notebooks",
+    "Tenis",
+  ]);
+
+  const [groupSelected, setGroupSelected] = useState("costas");
   return (
     <View style={styles.container}>
-      <SearchProducts />
       <StatusBar style="auto" />
+      <SearchProducts />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={
+              groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()
+            }
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
+      <Image source={BANNER} style={styles.bannerImg} />
+      <DealsOffers />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#f1f5f9",
+    backgroundColor: "#EFF2F4",
     alignItems: "center",
     justifyContent: "center",
+  },
+  bannerImg: {
+    height: 200,
+    width: "100%",
+    marginBottom: 8,
   },
 });
