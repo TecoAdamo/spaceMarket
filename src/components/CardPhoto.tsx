cfba0eae507bbc57bdb8d31e37838ad4b2492091
TecoAdamo <adamomateus478@gmail.com>
2024-05-31 03:29:51 -0300

    feat: card section for products

--- src/components/CardPhoto.tsx
+++ src/components/CardPhoto.tsx
@@ -1,13 +1,48 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  Image,
+  ImageProps,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { AuthNavigatorRoutesProps, ProductsProps } from "../routes/Auth.Routes";
+import React from "react";
 
-export default function CardPhoto() {
-  return <View style={styles.container}></View>;
+type Props = TouchableOpacityProps & {
+  data?: ProductsProps;
+};
+
+export default function CardPhoto({ data, ...rest }: Props) {
+  const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <TouchableOpacity style={styles.buttonBtn} {...rest}>
+        {data && data.thumbnail && (
+          <Image source={data.thumbnail} style={styles.imgBlazer} />
+        )}
+      </TouchableOpacity>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: 4,
-    width: "100%",
-    backgroundColor: "#FFFFFF",
+  buttonBtn: {
+    borderRadius: 15,
+  },
+  imgBlazer: {
+    borderRadius: 10,
+    marginBottom: 6,
+  },
+
+  textTitle: {
+    marginLeft: 10, // Adicione esta linha para dar espaço entre a imagem e o texto
   },
 });
--- src/components/DealsOffers.tsx
+++ src/components/DealsOffers.tsx
@@ -1,21 +1,54 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  ScrollView,
+  SectionList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+} from "react-native";
+import { MENU } from "../utils";
+import CardPhoto from "./CardPhoto";
+import { ProductsProps } from "../routes/Auth.Routes";
 
-export default function DealsOffers() {
+type Props = TouchableOpacityProps & {
+  data?: ProductsProps;
+};
+
+export default function DealsOffers({ data }: Props) {
   return (
     <View style={styles.container}>
       <View style={styles.textBox}>
         <Text style={styles.textTitle}>Promoções e ofertas</Text>
         <Text style={styles.subTitle}>Equipamentos Eletrônicos</Text>
       </View>
+
+      <SectionList
+        horizontal
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => <CardPhoto data={item} />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.textTitle}>{data?.title}</Text>
+        )}
+        contentContainerStyle={{ paddingBottom: 20 }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 4,
-    width: "100%",
     backgroundColor: "#FFFFFF",
+    width: "100%",
+    height: "50%",
+  },
+  containerText: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: "700",
+    marginLeft: 12,
   },
   textBox: {
     marginLeft: "8%",
@@ -30,7 +63,13 @@ const styles = StyleSheet.create({
     color: "gray",
   },
   cardPhoto: {
-    marginTop: 14,
     backgroundColor: "#979797",
+  },
+
+  priceText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
+    marginTop: 20,
   },
 });
