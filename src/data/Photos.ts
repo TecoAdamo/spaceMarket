cf049c1e0f233d845c68125679791c185f595382
TecoAdamo <adamomateus478@gmail.com>
2024-06-04 03:03:32 -0300

    .

--- src/data/Photos.ts
+++
@@ -1,8 +0,0 @@
-const PHOTOS = {
-  image1: require("../assets/blazer.png"),
-  image2: require("../assets/headset.jpg"),
-  image3: require("../assets/macbook.jpg"),
-  image4: require("../assets/relógio.jpg"),
-};
-
-export default PHOTOS;
--- src/data/index.ts
+++
@@ -1,8 +0,0 @@
-import PHOTO1 from "../assets/blazer.png";
-
-export const products = [
-  { id: "1", image: PHOTO1 },
-  // { id: "2", image: PHOTOS.image2 },
-  // { id: "3", image: PHOTOS.image3 },
-  // Adicione mais produtos conforme necessário
-];
