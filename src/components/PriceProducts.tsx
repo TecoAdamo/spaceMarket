136425390d97873f992b03cb3a95d7cb11bbcd7a
TecoAdamo <adamomateus478@gmail.com>
2024-06-04 03:01:52 -0300

    feat: component of price for products created.

---
+++ src/components/PriceProducts.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+type Props = {
+  price: number;
+};
+
+export default function PriceProducts({ price }: Props) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity>
+        <Text style={styles.textPrice}>R$ {price}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#d9f99d",
+    padding: 10,
+    borderRadius: 20,
+    width: "70%",
+    marginTop: 8,
+    marginBottom: 4,
+  },
+  textPrice: {
+    textAlign: "center",
+    color: "#00B517",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+});
