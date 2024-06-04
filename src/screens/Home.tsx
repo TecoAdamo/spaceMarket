976f39539b07a7f66a2b534ec0d715433f0ff58d
TecoAdamo <adamomateus478@gmail.com>
2024-06-04 03:02:17 -0300

    feat: added utils in home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-
 import { StatusBar } from "expo-status-bar";
 import {
   StyleSheet,
@@ -10,10 +9,14 @@ import {
   ScrollView,
 } from "react-native";
 import SearchProducts from "../components/SearchProducts";
-
 import BANNER from "../assets/banner.png";
 import { Group } from "../components/Groups";
 import DealsOffers from "../components/DealsOffers";
+import HomeProducts from "../components/HomeProducts";
+import { MENU as HomeProductsMenu } from "../utils/HomeProductsDB";
+import { MENU as DealsOffersMenu } from "../utils/DealsOffersDB";
+import { MENU as OtherProductsMenu } from "../utils/EletronicsDB";
+import Eletronics from "../components/Eletronics";
 
 export default function Home() {
   const [groups, setGroups] = useState([
@@ -25,6 +28,17 @@ export default function Home() {
   ]);
 
   const [groupSelected, setGroupSelected] = useState("costas");
+
+  const dealsOffersProducts = DealsOffersMenu.flatMap(
+    (section) => section.data
+  ).slice(0, 3);
+  const homeProductsItems = HomeProductsMenu.flatMap(
+    (section) => section.data
+  ).slice(0, 3);
+  const otherProductsItems = OtherProductsMenu.flatMap(
+    (section) => section.data
+  ).slice(0, 3);
+
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
@@ -49,7 +63,13 @@ export default function Home() {
           showsHorizontalScrollIndicator={false}
         />
         <Image source={BANNER} style={styles.bannerImg} />
-        <DealsOffers />
+        <DealsOffers
+          title="Promoções e ofertas"
+          subTitle="Equipamentos Eletrônicos"
+          products={dealsOffersProducts}
+        />
+        <HomeProducts title="Casa e exterior" products={homeProductsItems} />
+        <Eletronics title="Eletrônicos" sections={OtherProductsMenu} />
       </View>
     </ScrollView>
   );
@@ -65,6 +85,5 @@ const styles = StyleSheet.create({
     height: 200,
     width: "100%",
     marginBottom: 8,
-    marginTop: 18,
   },
 });
